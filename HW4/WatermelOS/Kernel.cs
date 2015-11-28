95c10679354d6d6099f4a36a35899543e70502b1
eeastwood <eastwoodeddy@aol.com>
2015-11-28 17:54:47 -0600

    Adding HW4, Cosmos_Kernal Project

---
+++ HW4/WatermelOS/Kernel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+
+namespace WatermelOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            HardwareSys.HardwareSys.Init();
+        }
+
+        protected override void Run()
+        {
+            HardwareSys.HardwareSys.Run(); 
+        }
+    }
+}
---
+++ HW4_CosmosKernal/WatermelOS/HardwareSys/HardwareSysLayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareSys
+{
+    public static class HardwareSys
+    {
+        public static void Init()
+        {
+            Hardware.Hardware.Init();
+        }
+
+        public static void Run()
+        {
+            Hardware.Hardware.Run(); 
+        }
+    }
+}
